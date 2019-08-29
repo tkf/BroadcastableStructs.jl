d2c9503068c3a872be87f7891bcca5a452d66ca8
Takafumi Arakaki <aka.tkf@gmail.com>
2019-08-28 20:43:37 -0700

    Avoid `Non-differentiable function getfield` error from Zygote

--- src/BroadcastableStructs.jl
+++ src/BroadcastableStructs.jl
@@ -3,6 +3,7 @@ module BroadcastableStructs
 export BroadcastableStruct, BroadcastableCallable
 
 using Setfield: constructor_of
+using ZygoteRules: @adjoint
 
 abstract type BroadcastableStruct end
 
@@ -11,10 +12,10 @@ fieldvalues(obj) = ntuple(i -> getfield(obj, i), fieldcount(typeof(obj)))
 Broadcast.broadcastable(obj::BroadcastableStruct) =
     Broadcast.broadcasted(constructor_of(typeof(obj)), fieldvalues(obj)...)
 
-#=
 Base.ndims(T::Type{<:BroadcastableStruct}) =
     mapreduce(ndims, max, fieldtypes(T); init=0)
 
+#=
 Base.axes(obj::BroadcastableStruct) = axes(Broadcast.broadcastable(obj))
 Base.length(obj::BroadcastableStruct) = prod(length.(axes(obj)))
 
@@ -28,5 +29,9 @@ call(f, args...) = f(args...)
 
 @inline Broadcast.broadcasted(c::BroadcastableCallable, args...) =
     Broadcast.broadcasted(call, c, args...)
+
+@adjoint fieldvalues(obj::T) where T = fieldvalues(obj), function(v)
+    (NamedTuple{fieldnames(T)}(v),)
+end
 
 end # module
