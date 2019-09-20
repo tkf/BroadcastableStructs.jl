7f98c0a56a4d3220f74accea894193236479a01b
Takafumi Arakaki <aka.tkf@gmail.com>
2019-09-19 23:16:50 -0700

    Test with nested BroadcastableCallable

--- test/preamble.jl
+++ test/preamble.jl
@@ -23,6 +23,24 @@ ManyAdd() = ManyAdd(1:5...)
 (f::ManyAdd)(y1, y2, y3, y4, y5) =
     f.x1 * y1 + f.x2 * y2 + f.x3 * y3 + f.x4 * y4 + f.x5 * y5
 
+struct Poly3{T0, T1, T2, T3} <: BroadcastableCallable
+    c0::T0
+    c1::T1
+    c2::T2
+    c3::T3
+end
+
+poly(c0=false, c1=false, c2=false, c3=false) = Poly3(c0, c1, c2, c3)
+
+(p::Poly3)(x) = p.c0 + p.c1 * x + p.c2 * x^2 + p.c3 * x^3
+
+struct AddCall{F, G} <: BroadcastableCallable
+    f::F
+    g::G
+end
+
+(c::AddCall)(x) = c.f(x) + c.g(x)
+
 macro test_inferred(ex)
     esc(:($Test.@test (($Test.@inferred $ex); true)))
 end
--- test/test_zygote.jl
+++ test/test_zygote.jl
@@ -10,4 +10,9 @@ f1 = WeightedAdd([1, 10], 2)
 @test gradient((x, y) -> sum(f0.(x, y)), [1, 10], 100) == ([1, 1], 4)
 @test gradient((x, y) -> sum(f1.(x, y)), [1, 10], 100) == ([1, 10], 4)
 
+c0 = poly([1, 10], [2, 20])
+c1 = AddCall(poly([1, 10], 0), poly(0, [2, 20]))
+@test gradient(x -> sum(c0.(x)), [1, 2]) == ([2, 20],)
+@test gradient(x -> sum(c1.(x)), [1, 2]) == ([2, 20],)
+
 end  # module
